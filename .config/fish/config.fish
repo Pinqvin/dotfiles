78f8460401e09298d37e6c818233457a2eff419e
Juuso Tapaninen <juuso.tapaninen@gmail.com>
2022-07-28 21:39:10 +0300

    Add adsf init to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,7 @@
 fish_add_path /opt/homebrew/bin ~/Library/Python/3.8/bin
 
+# Setup asdf
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
 # Keep as the last line of config
 starship init fish | source
