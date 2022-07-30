42b6546e01b3d23c4060a0fbb57c1db9cfc8dbad
Juuso Tapaninen <juuso.tapaninen@gmail.com>
2022-07-30 17:03:38 +0300

    Update PKG_CONFIG_PATH
    Add zlib pkg-config path. Remove SQLITE variable definitions as
    unnecessary

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,9 +4,7 @@ fish_add_path /opt/homebrew/bin ~/Library/Python/3.8/bin
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
 # Make pkg-config aware of homebrew installed sqlite
-set -x PKG_CONFIG_PATH /opt/homebrew/opt/sqlite/lib/pkgconfig
-set SQLITE_CFLAGS /opt/homebrew/opt/sqlite/include
-set SQLITE_LIBS /opt/homebrew/opt/sqlite/lib
+set -x PKG_CONFIG_PATH "/opt/homebrew/opt/sqlite/lib/pkgconfig:/opt/homebrew/opt/sqlite/lib/pkgconfig"
 
 # Keep as the last line of config
 starship init fish | source
