06a563131013826199a0b6aa0b27b440abc6bfc9
Juuso Tapaninen <juuso.tapaninen@gmail.com>
2022-07-30 16:55:13 +0300

    Define SQLITE_CFLAGS and SQLITE_LIBS environment variables
    Trying to get asdf PHP install working, still complaining about
    missing sqlite dependency

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,9 @@ fish_add_path /opt/homebrew/bin ~/Library/Python/3.8/bin
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
 # Make pkg-config aware of homebrew installed sqlite
-set PKG_CONFIG_PATH "/opt/homebrew/opt/sqlite/lib/pkgconfig"
+set PKG_CONFIG_PATH /opt/homebrew/opt/sqlite/lib/pkgconfig
+set SQLITE_CFLAGS /opt/homebrew/opt/sqlite/include
+set SQLITE_LIBS /opt/homebrew/opt/sqlite/lib
 
 # Keep as the last line of config
 starship init fish | source
