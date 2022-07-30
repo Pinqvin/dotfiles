027cd3c89a24c28f41eff8d49b722d4de304da88
Juuso Tapaninen <juuso.tapaninen@gmail.com>
2022-07-30 21:14:55 +0300

    Add sail alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,5 +6,7 @@ source /opt/homebrew/opt/asdf/libexec/asdf.fish
 # Make pkg-config aware of homebrew installed sqlite
 set -x PKG_CONFIG_PATH "/opt/homebrew/opt/sqlite/lib/pkgconfig:/opt/homebrew/opt/zlib/lib/pkgconfig:/opt/homebrew/opt/curl/lib/pkgconfig"
 
+alias sail="bash vendor/bin/sail"
+
 # Keep as the last line of config
 starship init fish | source
