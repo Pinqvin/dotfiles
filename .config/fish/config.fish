d152c716dd6e41d0775804ce46ab4e3dec9eb1e3
Juuso Tapaninen <juuso.tapaninen@gmail.com>
2022-07-30 13:56:29 +0300

    Add PKG_CONFIG_PATH for homebrew sqlite

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,5 +3,8 @@ fish_add_path /opt/homebrew/bin ~/Library/Python/3.8/bin
 # Setup asdf
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
+# Make pkg-config aware of homebrew installed sqlite
+PKG_CONFIG_PATH="/opt/homebrew/opt/sqlite/lib/pkgconfig"
+
 # Keep as the last line of config
 starship init fish | source
