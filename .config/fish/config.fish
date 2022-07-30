5241a21f5ae5a04b4645e42093869334dafd8b5a
Juuso Tapaninen <juuso.tapaninen@gmail.com>
2022-07-30 13:59:40 +0300

    Fix environment variable syntax

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,7 @@ fish_add_path /opt/homebrew/bin ~/Library/Python/3.8/bin
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
 # Make pkg-config aware of homebrew installed sqlite
-PKG_CONFIG_PATH="/opt/homebrew/opt/sqlite/lib/pkgconfig"
+set PKG_CONFIG_PATH "/opt/homebrew/opt/sqlite/lib/pkgconfig"
 
 # Keep as the last line of config
 starship init fish | source
