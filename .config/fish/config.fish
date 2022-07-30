ad85e1b88879c2ef867ea5bcaef40f4ecd1ff6c5
Juuso Tapaninen <juuso.tapaninen@gmail.com>
2022-07-30 20:44:41 +0300

    Add curl to PKG_CONFIG_PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,7 @@ fish_add_path /opt/homebrew/bin ~/Library/Python/3.8/bin
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
 # Make pkg-config aware of homebrew installed sqlite
-set -x PKG_CONFIG_PATH "/opt/homebrew/opt/sqlite/lib/pkgconfig:/opt/homebrew/opt/zlib/lib/pkgconfig"
+set -x PKG_CONFIG_PATH "/opt/homebrew/opt/sqlite/lib/pkgconfig:/opt/homebrew/opt/zlib/lib/pkgconfig:/opt/homebrew/opt/curl/lib/pkgconfig"
 
 # Keep as the last line of config
 starship init fish | source
